7901126bc6b6828e5f6901c2e2cc2ba9bf0b1c04
clase 1 <albertoarauzsalazar65@gmail.com>
2023-03-15 20:14:37 -0600

    practica 2

--- clase1.py
+++ clase1.py
@@ -125,11 +125,16 @@ print(texto_enlace_whatsapp)
 
 print("--------------")
 # pruebas practica 1
-# ejercicio numero 1
+# ejercicio numero 1 Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
 print("hola mundo")
 
-# ejercicio 2
+# ejercicio 2 Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
 mensaje="¡hola mundo!"
 print(mensaje)
+
+# Ejercicio 3
+
+# Escribir un programa que almacene el nombre del usuario en la consola y después de que muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario.
+
 
 
