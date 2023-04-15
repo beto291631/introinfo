f491338bd7ff0c1c4cc7d71044c7ce44e22dff2e
clase 1 <albertoarauzsalazar65@gmail.com>
2023-04-15 15:34:52 -0600

    aprendisaje

---
+++ practica.py
@@ -0,0 +1,10 @@
+# Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+
+mensaje = "¡Hola mundo!"
+print("¡hola mundo!")
+
+# Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+
+nombre = input("¿Cómo te llamas? ")
+print("¡Hola " + nombre + "!")
+
